9c6deb342a8e9ba147286e876f2a94402eb5ebca
Devanand K <devanandk3319@gmail.com>
2026-01-31 15:33:54 +0530

    Add files via upload

---
+++ max_min.py
@@ -0,0 +1,26 @@
+d = {1:2,2:3,1:5,5:2}
+maximum=max(d.values())
+minimum=min(d.values())
+print("maximum element",maximum)
+print("minimum element",minimum)	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
